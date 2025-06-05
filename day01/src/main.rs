5afe2d907e224cbd4a4dde7be74da561ffdb21e7
Bernhard Kinast <kiniber88@gmail.com>
2025-06-05 21:36:50 +0200

    Better formatting

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -5,10 +5,8 @@ use part1::*;
 
 fn main() -> anyhow::Result<()> {
     println!("Advent of code 2016: 1.1");
-
     let input_1_1 = include_str!("../resources/input_1_1.txt");
-    
     part1(input_1_1)?;
-
+    // TODO: 1.2
     Ok(())
 }
--- day01/src/part1.rs
+++ day01/src/part1.rs
@@ -1,7 +1,5 @@
 use thiserror::Error;
 
-
-
 pub fn part1(input: &str) -> anyhow::Result<()> {
     // println!("{input}");
     let parts = input
@@ -10,13 +8,16 @@ pub fn part1(input: &str) -> anyhow::Result<()> {
         .filter_map(|res| res.ok())
         .collect::<Vec<_>>();
     // println!("{:?}", parts);
-    let mut position = (0,0);
-    let mut direction = (0,1);
+    let mut position = (0, 0);
+    let mut direction = (0, 1);
     for path_fragmen in parts {
         append_next_path_fragment(&mut position, &mut direction, &path_fragmen);
     }
     let distance = position.0.abs() + position.1.abs();
-    println!("final position: ({}, {}, {}): ", position.0, position.1, distance);
+    println!(
+        "final position: ({}, {}, {}): ",
+        position.0, position.1, distance
+    );
     Ok(())
 }
 
@@ -29,9 +30,9 @@ struct PathFragment {
 #[derive(Error, Debug)]
 enum Day1ApplicationError {
     #[error("The direction '{unknown_direction}' is not implemented")]
-    UnknownDirectionError{unknown_direction: String},
+    UnknownDirectionError { unknown_direction: String },
     #[error("The moving direct ({x}, {y}) is not implemented")]
-    UnknownMovingDirection{x: i16, y:i16},
+    UnknownMovingDirection { x: i16, y: i16 },
 }
 
 fn try_parse_path_fragment(input: &str) -> anyhow::Result<PathFragment> {
@@ -39,13 +40,26 @@ fn try_parse_path_fragment(input: &str) -> anyhow::Result<PathFragment> {
     let value = &input[1..];
     let value = value.parse::<i16>()?;
     match direction {
-        "R" => Ok(PathFragment{x:value, ..Default::default()}),
-        "L" => Ok(PathFragment{x:-value, ..Default::default()}),
-        unknown_direction => Err(Day1ApplicationError::UnknownDirectionError { unknown_direction: unknown_direction.to_string() }.into())
+        "R" => Ok(PathFragment {
+            x: value,
+            ..Default::default()
+        }),
+        "L" => Ok(PathFragment {
+            x: -value,
+            ..Default::default()
+        }),
+        unknown_direction => Err(Day1ApplicationError::UnknownDirectionError {
+            unknown_direction: unknown_direction.to_string(),
+        }
+        .into()),
     }
 }
 
-fn append_next_path_fragment(global_position: &mut (i16, i16), current_direction: &mut (i16, i16), fragment_to_append: &PathFragment){
+fn append_next_path_fragment(
+    global_position: &mut (i16, i16),
+    current_direction: &mut (i16, i16),
+    fragment_to_append: &PathFragment,
+) {
     //let mut nextdirection = (0i8, 0i8);
 
     // println!("({global_position:?}, {current_direction:?}, {fragment_to_append:?})");
@@ -80,13 +94,12 @@ fn append_next_path_fragment(global_position: &mut (i16, i16), current_direction
             current_direction.1 = fragment_to_append.x.signum();
         }
         (x, y) => {
-            let err = Day1ApplicationError::UnknownMovingDirection{x:x.clone(), y:y.clone()};
+            let err = Day1ApplicationError::UnknownMovingDirection {
+                x: x.clone(),
+                y: y.clone(),
+            };
+            // TODO: throw the Error instead of just printing it
             println!("{err}");
         }
-
-
-
     }
-
-
 }
