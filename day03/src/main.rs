03b972e6c13573dd09d294f5fe016e79af0d50e6
Bernhard Kinast <kiniber88@gmail.com>
2025-06-06 18:32:54 +0200

    Implemented 3.2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,10 +1,14 @@
 mod part1;
+mod part2;
 
 use part1::part1;
+use part2::part2;
 
 fn main() -> anyhow::Result<()> {
     println!("Advent of code 2016: 3.1");
     let input = include_str!("../resources/input.txt");
     part1(input)?;
+    println!("Advent of code 2016: 3.2");
+    part2(input)?;
     Ok(())
 }
---
+++ day03/src/part2.rs
@@ -0,0 +1,57 @@
+use itertools::Itertools;
+
+pub fn part2(input: &str) -> anyhow::Result<()> {
+
+    let x = input.split_whitespace().collect::<Vec<_>>();
+    let chunked = x.into_iter().chunks(9);
+    let chunked = chunked.into_iter().collect::<Vec<_>>();
+    let all_shapes = chunked.into_iter().map(|mut chunk|{
+        let a1 = chunk.next().unwrap();
+        let a2 = chunk.next().unwrap();
+        let a3 = chunk.next().unwrap();
+        let a4 = chunk.next().unwrap();
+        let a5 = chunk.next().unwrap();
+        let a6 = chunk.next().unwrap();
+        let a7 = chunk.next().unwrap();
+        let a8 = chunk.next().unwrap();
+        let a9 = chunk.next().unwrap();
+
+        vec![
+            (a1, a4, a7),
+            (a2, a5, a8),
+            (a3, a6, a9),
+        ]
+    }).flatten()
+    // convert the strings to numbers and keep the tuple form
+    .map_while(|(a,b,c)|{
+        match (a.parse::<i32>(), b.parse::<i32>(), c.parse::<i32>()) {
+            // if all 3 conversions were ok, we have a result
+            (Ok(a), Ok(b), Ok(c)) => 
+            {
+                let mut v = [a, b, c];
+                v.sort(); // We sort the values
+                let [a,b,c] = v;
+                Some((a,b,c))
+            },
+            // Since we use map_while, None values will be removed from the stream
+            _ => None
+        }
+    }).collect::<Vec<_>>();
+ 
+    //println!("{all_shapes:?}");
+    let amount_triangles = all_shapes.iter().filter(ValidateTriangle::is_triangle).count();
+    println!("Found {amount_triangles} triangles");
+    Ok(())
+}
+
+trait ValidateTriangle {
+    fn is_triangle(&self) -> bool;
+}
+
+impl ValidateTriangle for &(i32, i32, i32) {
+    fn is_triangle(&self) -> bool {
+        // We sorted earlier, so c is the biggest
+        let (a,b,c) = self;
+        c - a - b < 0
+    }
+}
