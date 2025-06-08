c3309a3db4451d4ddc29bcb2cf15140f56af9a11
Bernhard Kinast <kiniber88@gmail.com>
2025-06-08 22:09:36 +0200

    Solved 6.2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,7 +1,7 @@
 use std::time::Instant;
 
 mod part1;
-// mod part2;
+mod part2;
 
 fn main() -> anyhow::Result<()> {
     println!("Advent of code 2016: 6.1");
@@ -10,12 +10,12 @@ fn main() -> anyhow::Result<()> {
     part1::part1(input)?;
     let duration = start_time.elapsed();
     println!("6.1 Time elapsed: {duration:?}");
-    /*
+    
     println!("Advent of code 2016: 6.2");
     let start_time = Instant::now();
     part2::part2(input)?;
     let duration = start_time.elapsed();
     println!("6.2 Time elapsed (parallel): {duration:?}");
-    */
+    
     Ok(())
 }
---
+++ day06/src/part2.rs
@@ -0,0 +1,25 @@
+use std::collections::{BTreeMap, HashMap};
+
+pub fn part2(input: &str) -> anyhow::Result<()> {
+    let mut map: BTreeMap<usize, HashMap<char,usize>> = BTreeMap::new();
+    let mut column_index;
+    for line in input.split("\n") {
+        column_index = 0;
+        let mut chars = line.chars();
+        while let Some(chr) = chars.next() {
+            let char_count_map = map.entry(column_index).or_insert(HashMap::new());
+            let char_count_vec = char_count_map.entry(chr).or_insert(0);
+            *char_count_vec += 1;
+            column_index +=1;
+        }
+    }
+    let mut result = String::new();
+    for column in map {
+        let min = column.1.iter().min_by_key(|e|e.1);
+        if let Some((chr, _)) = min {
+            result.push(*chr);
+        }
+    }
+    println!("{result}");
+    Ok(())
+}
